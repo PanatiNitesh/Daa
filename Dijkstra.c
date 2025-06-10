5a620f452e04a3da5a98fc17eaf601a353c2aa1c
Nitesh <134051960+PanatiNitesh@users.noreply.github.com>
2025-06-10 19:35:30 +0530

    Update Dijkstra.c

--- Dijkstra.c
+++ Dijkstra.c
@@ -21,7 +21,7 @@ void Dijkstra(int start) {
         min = INF;
         u = -1;
 
-        // Find the unvisited vertex with the smallest distance
+        // Find the unvisited vertex
         for (j = 1; j <= n; j++) {
             if (!visited[j] && d[j] < min) {
                 min = d[j];
